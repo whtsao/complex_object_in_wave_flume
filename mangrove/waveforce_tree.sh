8f0e036edac890a37f05b359af432f1bf3ec5c7b
Wen-Huai Tsao <87154971+whtsao@users.noreply.github.com>
2024-01-15 09:25:27 -0600

    fix fresh-water tank

--- mangrove/waveforce_tree.sh
+++ mangrove/waveforce_tree.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-#SBATCH -N 4
-#SBATCH -n 256
-#SBATCH -t 00:30:00
+#SBATCH -N 2
+#SBATCH -n 128
+#SBATCH -t 00:10:00
 #SBATCH -p workq
 #SBATCH -A hpc_ceds3d
-#SBATCH -J mangrove_T1
+#SBATCH -J lidar_mangrove
 #SBATCH -o o.out
 #SBATCH -e e.err
 #load proteus module and ensure proteus's python is in path
 date
+
 module purge
 module load intel/2021.5.0 
 module load mvapich2/2.3.7/intel-2021.5.0   
@@ -16,15 +17,16 @@ module load gcc/11.2.0
 module load proteus/fct
 export LD_LIBRARY_PATH=/home/packages/compilers/intel/compiler/2022.0.2/linux/compiler/lib/intel64_lin:${LD_LIBRARY_PATH}
 export MV2_HOMOGENEOUS_CLUSTER=1
+
 mkdir -p $WORK/$SLURM_JOB_NAME.$SLURM_JOBID 
 cd "$SLURM_SUBMIT_DIR"
-cp setup.py $WORK/$SLURM_JOB_NAME.$SLURM_JOBID
-#cp mangrove_1.pyx $WORK/$SLURM_JOB_NAME.$SLURM_JOBID
-cp mangrove.pyx $WORK/$SLURM_JOB_NAME.$SLURM_JOBID
-cp waveforce_tree.py $WORK/$SLURM_JOB_NAME.$SLURM_JOBID
-cp waveforce_tree.sh $WORK/$SLURM_JOB_NAME.$SLURM_JOBID
+cp *.pyx $WORK/$SLURM_JOB_NAME.$SLURM_JOBID
+cp *.py $WORK/$SLURM_JOB_NAME.$SLURM_JOBID
+cp *.sh $WORK/$SLURM_JOB_NAME.$SLURM_JOBID
 cp *.stl $WORK/$SLURM_JOB_NAME.$SLURM_JOBID
 cd $WORK/$SLURM_JOB_NAME.$SLURM_JOBID
+
 python setup.py build_ext -i
-srun parun --TwoPhaseFlow waveforce_tree.py -F -l 5 -C "he=0.1"
+srun parun --TwoPhaseFlow waveforce_tree.py -F -l 5 -C "he=0.4"
+
 exit 0
