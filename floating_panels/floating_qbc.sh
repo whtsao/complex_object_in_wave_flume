259a6d2ec7e7a9a9fd9730b304241ba56d267260
whtsao <philodihowniton@gmail.com>
2024-01-21 19:29:25 -0600

    testing floating panels example

---
+++ floating_panels/floating_qbc.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -n 48
+#SBATCH -t 00:10:00
+#SBATCH -p workq
+#SBATCH -A loni_ceds3d
+#SBATCH -o o.out
+#SBATCH -e e.err
+#SBATCH -J floating_panels
+#load proteus module and ensure proteus's python is in path
+
+date
+
+module purge
+module load proteus/1.8.1
+
+mkdir -p $WORK/$SLURM_JOB_NAME.$SLURM_JOBID 
+cd $WORK/$SLURM_JOB_NAME.$SLURM_JOBID 
+cp $SLURM_SUBMIT_DIR/petsc.options.superlu_dist .
+cp $SLURM_SUBMIT_DIR/*.py .
+cp $SLURM_SUBMIT_DIR/*.sh .
+
+srun parun --TwoPhaseFlow floating_panels.py -F -l 5 -C "he=0.2" # all on
+#srun parun --TwoPhaseFlow floating_panels.py -F -l 5 -C "he=0.2 bodybool2=False linkbool=False" # only one panel
+#srun parun --TwoPhaseFlow floating_panels.py -F -l 5 -C "he=0.2 linkbool=False" # two panels unconnected
+
+exit 0
