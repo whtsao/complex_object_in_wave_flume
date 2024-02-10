720866933f07585d3068e2910119409da57ec10f
whtsao <philodihowniton@gmail.com>
2024-02-10 17:17:33 -0600

    first complete run

--- pvc_mangrove_in_waves/waveforce_tree.sh
+++ pvc_mangrove_in_waves/waveforce_tree.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH -N 4
-#SBATCH -n 256
-#SBATCH -t 00:10:00
+#SBATCH -N 16
+#SBATCH -n 1024
+#SBATCH -t 72:00:00
 #SBATCH -p workq
 #SBATCH -A hpc_ceds3d
 #SBATCH -J a_unit_pvc_mangrove
@@ -27,6 +27,6 @@ cp *.stl $WORK/$SLURM_JOB_NAME.$SLURM_JOBID
 cd $WORK/$SLURM_JOB_NAME.$SLURM_JOBID
 
 python setup.py build_ext -i
-srun parun --TwoPhaseFlow waveforce_tree.py -F -l 5 -C "he=0.05"
+srun parun --TwoPhaseFlow waveforce_tree.py -F -l 5 -C "he=0.02"
 
 exit 0
